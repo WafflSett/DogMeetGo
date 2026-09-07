cfd00fd95b79be383d8142a21606faa5e4316f0d
WafflSett <toros.gergo@gmail.com>
2026-09-07 19:40:14 +0200

    setup blank pages and routing

--- AppShell.xaml.cs
+++ AppShell.xaml.cs
@@ -1,10 +1,16 @@
-﻿namespace DogMeetGo
+﻿using DogMeetGo.Pages;
+
+namespace DogMeetGo
 {
     public partial class AppShell : Shell
     {
         public AppShell()
         {
             InitializeComponent();
+            Routing.RegisterRoute("walkdetails", typeof(WalkDetailsPage));
+            Routing.RegisterRoute("createdog", typeof(CreateDogPage));
+            Routing.RegisterRoute("createwalk", typeof(CreateWalkPage));
+            Routing.RegisterRoute("createprofile", typeof(CreateProfilePage));
         }
     }
 }
